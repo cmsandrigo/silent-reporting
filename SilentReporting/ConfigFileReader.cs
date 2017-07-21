849db2ffddd6e9e5d797a858aeda5869524ddc56
César M. Sandrigo <cmsandrigo@gmail.com>
2017-07-21 18:39:18 -0300

    Initial commit with the a very first approach

---
+++ SilentReporting/ConfigFileReader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilentReporting
+{
+    public class ConfigFileReader
+    {
+        private string _absolutePath;
+
+        public ConfigFileReader(string path)
+        {
+            _absolutePath = Path.IsPathRooted(path) ? path : Path.GetFullPath(path);
+
+            if (!File.Exists(_absolutePath))
+                throw new ArgumentException("El archivo específicado no existe");
+        }
+
+        public Dictionary<string, string> GetAll()
+        {
+            var allConfigValues = new Dictionary<string, string>();
+            var lines = File.ReadAllLines(_absolutePath);
+
+            foreach (var line in lines)
+            {
+                int equalIndex = line.IndexOf('=');
+                string optName = line.Substring(1, equalIndex);
+                string optValue = line.Substring(equalIndex);
+
+                allConfigValues.Add(optName, optValue);
+            }
+
+            return allConfigValues;
+        }
+    }
+}
---
+++ SilentReporting/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SilentReporting
+{
+    static class Program
+    {
+        private const string FILE_PARAMETER_NAME = "f";
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            
+            var args = Environment.GetCommandLineArgs();
+
+            if (CommandLineUtil.HasParameter(args, ))
+
+            Application.Run(new Form1());
+        }
+    }
+}
